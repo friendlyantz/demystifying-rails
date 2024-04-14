fc0bd6e04c96c9d39778ccd94a03bbcd01061d0f
anton <friendlyantz@pm.me>
2024-04-15 00:03:59 +1000

    hash_with_indifferent_access

--- active_support/lib/app.rb
+++ active_support/lib/app.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
-require "active_support"
-
 class App
   def initialize
-    puts "App initialized".red
+    hash_with_indifferent_access
+  end
+
+  def hash_with_indifferent_access
+    puts "Hash with indifferent access".red
+    require "active_support/core_ext/hash/indifferent_access"
+    hash = {a: "hash_key"}.with_indifferent_access
+    puts hash["a"]
+    puts hash[:a]
+
+    try = {a: "key not founfd"}["a"] # => 1w
+    puts try
+    try_2 = {a: "value"}[:a] # => 1w
+    puts try_2
+    result = {a: "another value"}.with_indifferent_access["a"]
+    puts result
   end
 end
